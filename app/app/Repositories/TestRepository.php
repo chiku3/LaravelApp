f001c21b4f8b06b4deee17fc02a0a163d0224932
chiku3 <p314chiku.y@gmail.com>
2022-07-08 12:13:26 +0900

    ADD[generater]

---
+++ app/app/Repositories/TestRepository.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Test;
+use App\Repositories\BaseRepository;
+
+/**
+ * Class TestRepository
+ * @package App\Repositories
+ * @version July 8, 2022, 11:39 am JST
+*/
+
+class TestRepository extends BaseRepository
+{
+    /**
+     * @var array
+     */
+    protected $fieldSearchable = [
+        
+    ];
+
+    /**
+     * Return searchable fields
+     *
+     * @return array
+     */
+    public function getFieldsSearchable()
+    {
+        return $this->fieldSearchable;
+    }
+
+    /**
+     * Configure the Model
+     **/
+    public function model()
+    {
+        return Test::class;
+    }
+}
